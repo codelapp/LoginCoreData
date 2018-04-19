59118c48f8f9f6b2c4343696105c06553129184a
codelapp <info@codelapp.com>
2018-04-19 18:09:01 +0200

    Commit final

--- LoginCoreData/AppDelegate.swift
+++ LoginCoreData/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  LoginCoreData
-//
-//  Created by Jorge Maldonado Borbón on 28/11/17.
-//  Copyright © 2017 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
 import CoreData
 
--- LoginCoreData/ViewController.swift
+++ LoginCoreData/ViewController.swift
@@ -1,25 +1,95 @@
-//
-//  ViewController.swift
-//  LoginCoreData
-//
-//  Created by Jorge Maldonado Borbón on 28/11/17.
-//  Copyright © 2017 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var user: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    
+    func conexion()->NSManagedObjectContext{
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        return delegate.persistentContainer.viewContext
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func entrar(_ sender: UIButton) {
+        let contexto = conexion()
+        let fetchRequest : NSFetchRequest<Login> = Login.fetchRequest()
+        let predicado = NSPredicate(format: "user == %@ AND pass == %@", user.text!, pass.text!)
+        fetchRequest.predicate = predicado
+        
+        do {
+            let resultado = try contexto.fetch(fetchRequest)
+            
+            if resultado.count == 1 {
+                performSegue(withIdentifier: "entrar", sender: self)
+            }else{
+                print("no entro")
+            }
+            
+        } catch let error as NSError {
+            print("hubo un error en el login", error)
+        }
+        
     }
+    
+    @IBAction func registrar(_ sender: UIButton) {
+        let contexto = conexion()
+        
+        let entityLogin = NSEntityDescription.entity(forEntityName: "Login", in: contexto)
+        let newLogin = NSManagedObject(entity: entityLogin!, insertInto: contexto)
+        
+        newLogin.setValue("pepe", forKey: "user")
+        newLogin.setValue("1234", forKey: "pass")
+        
+        do {
+            try contexto.save()
+            print("guardo correctamente")
+        } catch let error as NSError {
+            print("error al guardar", error)
+        }
+        
+    }
+    
+    
+    @IBAction func mostrar(_ sender: UIButton) {
+        let contexto = conexion()
+        let fetchRequest : NSFetchRequest<Login> = Login.fetchRequest()
+        
+        do {
+            
+            let resultados = try contexto.fetch(fetchRequest)
+            
+            print("numero de registros= \(resultados.count)")
+            
+            for res in resultados as [NSManagedObject] {
+                let user = res.value(forKey: "user")
+                let pass = res.value(forKey: "pass")
+                print("User: \(user!) - Pass: \(pass!)")
+            }
+            
+        } catch let error as NSError  {
+            print("no mostro nada", error)
+        }
 
-
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
 
